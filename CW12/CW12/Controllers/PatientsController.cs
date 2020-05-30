d1fba5e277f0c0c26b1f9c4c5b3b5ea9424e3dd5
s18986 <s18986@users.noreply.github.com>
2020-05-30 16:50:27 +0200

    zmiana projektu
    poprawienie koncowek i dodanie reagujacych przyciskow

---
+++ CW12/CW12/Controllers/PatientsController.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using CW12.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace CW12.Controllers
+{
+    public class PatientsController : Controller
+    {
+        private readonly s18986Context _context;
+        public PatientsController(Models.s18986Context context)
+        {
+            _context = context;
+        }
+        public IActionResult Index()
+        {
+            var temp = _context.Patients.ToList();
+            return View(temp);
+        }
+        public IActionResult GetDetails(Patients pat)
+        {
+            var db = new s18986Context();
+            var Recepty = db.Prescriptions.ToList().Where(d => d.IdPatient == pat.IdPatient);
+            ViewBag.Imie = pat.FirstName;
+            return View(Recepty);
+        }
+        public IActionResult Create()
+        {
+            return View();
+        }
+        [HttpPost]
+        public IActionResult Create(Patients pat)
+        {
+            if (!ModelState.IsValid)
+            {
+                return View(pat);
+            }
+            var db = new s18986Context();
+            db.Patients.Add(pat);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+        [HttpDelete]
+        public IActionResult Delete(Patients pat)
+        {
+            if (!ModelState.IsValid)
+            {
+                return View(pat);
+            }
+            var db = new s18986Context();
+            db.Patients.Remove(pat);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+    }
+}
---
+++ CW12/CW12/Models/PrescriptionMedicaments.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace CW12.Models
+{
+    public partial class PrescriptionMedicaments
+    {
+        public int IdPrescription { get; set; }
+        public int? Dose { get; set; }
+        public string Details { get; set; }
+        public int? PrescriptionIdPrescription { get; set; }
+        public int? MedicamentIdMedicament { get; set; }
+
+        public virtual Medicaments MedicamentIdMedicamentNavigation { get; set; }
+        public virtual Prescriptions PrescriptionIdPrescriptionNavigation { get; set; }
+    }
+}
